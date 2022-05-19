c0ed8ba09c122d195a136c8b1d78254deb3b089f
Marta <Marta@DESKTOP-KGQQT70>
2022-05-19 08:53:59 +0200

    Segundo código

--- src/trabajo/PeePeePooPoo_Principal.java
+++ src/trabajo/PeePeePooPoo_Principal.java
@@ -10,9 +10,9 @@ public class PeePeePooPoo_Principal {
 		System.out.println("Sólo tienes que decir 3 veces su nombre (Pee Pee Poo Poo)");
 
 		// METER "PEEPEEPOOPOO POR TECLADO
-
 		Scanner ent = new Scanner(System.in);
 		boolean correcto = false;
+		boolean completado = true;
 		do {
 			System.out.println("Adelante: ");
 			String nombre1 = ent.nextLine();
@@ -27,6 +27,7 @@ public class PeePeePooPoo_Principal {
 
 					if (nombre3.equalsIgnoreCase("Pee Pee Poo Poo Man")) {
 						correcto = true;
+						completado = true;
 
 					} else {
 
@@ -42,52 +43,51 @@ public class PeePeePooPoo_Principal {
 				System.out.println("Nombre incorrecto, el Pee Pee Poo Poo Man se está enfadando...");
 
 			}
-		} while (correcto = true);
+		} while (correcto == true && !completado);
 
+		
+		// CALCULADORA
+		Scanner ent2 = new Scanner(System.in);
+		int opcion = 0;
+		float op1 = 0.0f, op2 = 0.0f;
+
+		do {
+			System.out.println("Calculadora PeePeePooPoo: \n" + "1º) Suma \n" + "2º) Resta \n" + "3º) Multiplicación \n"
+					+ "4º) División \n" + "5º) Salir \n");
+
+			opcion = ent2.nextInt();
+
+			if (opcion >= 1 && opcion <= 4) {
+				System.out.println("Introduce un número:");
+				op1 = ent2.nextFloat();
+				System.out.println("Introduce el otro número:");
+				op2 = ent2.nextFloat();
+			}
+
+			switch (opcion) {
+			case 1:
+				sumar(op1, op2);
+				break;
+			case 2:
+				restar(op1, op2);
+				break;
+			case 3:
+				multiplicar(op1, op2);
+				break;
+			case 4:
+				dividir(op1, op2);
+				break;
+			case 5:
+				System.out.println("Fin");
+				break;
+			default:
+				System.out.println("Opción no válida.");
+			}
+
+		} while (opcion != 5);
+
+		System.out.println("Gracias por ayudar al PeePeePooPoo Man, jamás lo olvidará");
+
+		// FALTA CREAR LOS MÉTODOS CORRESPONDIENTES PARA PODER HACER LAS OPERACIONES :)
 	}
-
-	// CALCULADORA
-	Scanner ent2 = new Scanner(System.in);
-	int opcion = 0;
-	float op1 = 0.0f, op2 = 0.0f;
-
-	do
-	{
-		System.out.println("Calculadora PeePeePooPoo: \n" + "1º) Suma \n" + "2º) Resta \n" + "3º) Multiplicación \n"
-				+ "4º) División \n" + "5º) Salir \n");
-
-		opcion = ent2.nextInt();
-
-		if (opcion >= 1 && opcion <= 4) {
-			System.out.println("Introduce un número:");
-			op1 = ent2.nextFloat();
-			System.out.println("Introduce el otro número:");
-			op2 = ent2.nextFloat();
-		}
-
-		switch (opcion) {
-		case 1:
-			sumar(op1, op2);
-			break;
-		case 2:
-			restar(op1, op2);
-			break;
-		case 3:
-			multiplicar(op1, op2);
-			break;
-		case 4:
-			dividir(op1, op2);
-			break;
-		case 5:
-			System.out.println("Fin");
-			break;
-		default:
-			System.out.println("Opción no válida.");
-		}
-
-	}while(opcion!=5);
-
-	System.out.println("Gracias por ayudar al PeePeePooPoo Man, jamás lo olvidará");
-
-	// FALTA CREAR LOS MÉTODOS CORRESPONDIENTES PARA PODER HACER LAS OPERACIONES :)
-}}
+}
